4385a7abccffb59978723696dede6db82b56ca74
arthurpaturel <arthur.paturel@ynov.com>
2022-06-15 00:55:22 +0200

    trgvr

--- appli_meteo/lib/main.dart
+++ appli_meteo/lib/main.dart
@@ -6,6 +6,20 @@ void main() {
   runApp(const MyApp());
 }
 
+@override
+void initState() {
+  super.initState();
+  initialisation();
+}
+
+void initialisation() async {
+  getCityWeather("paris").then((value) => {
+        setState(() {
+          widget.meteo = value;
+        })
+      });
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
@@ -33,25 +47,214 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
-  void initState() {
-    super.initState();
-    initialisation();
-  }
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Stack(
+      children: [
+        Container(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/background/background_sunny.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        Column(children: [
+          Spacer(),
+          Column(
+            children: [
+              Text("Lyon", style: TextStyle(color: Colors.white, fontSize: 30)),
+              Text("26°", style: TextStyle(color: Colors.white, fontSize: 30)),
+              Text(""),
+              Text("Belles éclaircies",
+                  style: TextStyle(color: Colors.white, fontSize: 30)),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                Text("Max. 25°",
+                    style: TextStyle(color: Colors.white, fontSize: 30)),
+                Text(" | ",
+                    style: TextStyle(color: Colors.white, fontSize: 30)),
+                Text("Min. 12°",
+                    style: TextStyle(color: Colors.white, fontSize: 30))
+              ])
+            ],
+          ),
+          Spacer(),
+          Row(
+            children: [
+              Spacer(),
+              Container(
+                  decoration: const BoxDecoration(
+                    color: Color.fromRGBO(0, 102, 255, 0.25),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  child: SizedBox(
+                    height: 100,
+                    width: 70,
+                    child: Column(children: [
+                      Spacer(),
+                      Text("Lun", style: TextStyle(color: Colors.white)),
+                      Text("25°", style: TextStyle(color: Colors.white)),
+                      Image(
+                        height: 40,
+                        width: 40,
+                        image: AssetImage("assets/icons/sun.png"),
+                      ),
+                      Spacer()
+                    ]),
+                  )),
+              Spacer(),
+              Container(
+                  decoration: const BoxDecoration(
+                    color: Color.fromRGBO(0, 102, 255, 0.25),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  child: SizedBox(
+                    height: 100,
+                    width: 70,
+                    child: Column(children: [
+                      Spacer(),
+                      Text("Lun", style: TextStyle(color: Colors.white)),
+                      Text("25°", style: TextStyle(color: Colors.white)),
+                      Image(
+                        height: 40,
+                        width: 40,
+                        image: AssetImage("assets/icons/sun.png"),
+                      ),
+                      Spacer()
+                    ]),
+                  )),
+              Spacer(),
+              Container(
+                  decoration: const BoxDecoration(
+                    color: Color.fromRGBO(0, 102, 255, 0.25),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  child: SizedBox(
+                    height: 100,
+                    width: 70,
+                    child: Column(children: [
+                      Spacer(),
+                      Text("Lun", style: TextStyle(color: Colors.white)),
+                      Text("25°", style: TextStyle(color: Colors.white)),
+                      Image(
+                        height: 40,
+                        width: 40,
+                        image: AssetImage("assets/icons/sun.png"),
+                      ),
+                      Spacer()
+                    ]),
+                  )),
+              Spacer(),
+              Container(
+                  decoration: const BoxDecoration(
+                    color: Color.fromRGBO(0, 102, 255, 0.25),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  child: SizedBox(
+                    height: 100,
+                    width: 70,
+                    child: Column(children: [
+                      Spacer(),
+                      Text("Lun", style: TextStyle(color: Colors.white)),
+                      Text("25°", style: TextStyle(color: Colors.white)),
+                      Image(
+                        height: 40,
+                        width: 40,
+                        image: AssetImage("assets/icons/sun.png"),
+                      ),
+                      Spacer()
+                    ]),
+                  )),
+              Spacer(),
+              Container(
+                  decoration: const BoxDecoration(
+                    color: Color.fromRGBO(0, 102, 255, 0.25),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                  child: SizedBox(
+                    height: 100,
+                    width: 70,
+                    child: Column(children: [
+                      Spacer(),
+                      Text("Lun", style: TextStyle(color: Colors.white)),
+                      Text("25°", style: TextStyle(color: Colors.white)),
+                      Image(
+                        height: 40,
+                        width: 40,
+                        image: AssetImage("assets/icons/sun.png"),
+                      ),
+                      Spacer()
+                    ]),
+                  )),
+              Spacer(),
+            ],
+          ),
+          Spacer(),
+          Container(
+              height: 370,
+              width: 350,
+              decoration: const BoxDecoration(
+                color: Color.fromRGBO(0, 102, 255, 0.25),
+                borderRadius: BorderRadius.all(
+                  Radius.circular(10),
+                ),
+              ),
+              child: SizedBox(
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Image(
+                            height: 24,
+                            width: 24,
+                            image: AssetImage("assets/icons/calendar.png")),
+                        Text("Météo sur 24h",
+                            style: TextStyle(
+                                color: Color.fromRGBO(130, 186, 251, 1),
+                                fontSize: 16))
+                      ],
+                    ),
+                    Container(
+                      height: 1,
+                      width: 330,
+                      decoration: const BoxDecoration(
+                          color: Color.fromRGBO(130, 186, 251, 100)),
+                    ),
+                    Row(
+                      children: [
+                        Text("8h"),
+                        Image(
+                            height: 24,
+                            width: 24,
+                            image: AssetImage("assets/icons/sun.png")),
+                        Text("24°")
+                      ],
+                    )
+                  ],
+                ),
+              )),
+          Spacer()
+        ]),
+      ],
+    ));
 
-  void initialisation() async {
-    getCityWeather("paris").then((value) => {
-          setState(() {
-            widget.meteo = value;
-          })
-        });
-  }
-
-  @override
+    /*@override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: const Text("Météo")),
         body: widget.meteo == null
             ? const Center(child: CircularProgressIndicator())
             : Text(widget.meteo!.name ?? "IN LOAD"));
+  }*/
   }
 }
